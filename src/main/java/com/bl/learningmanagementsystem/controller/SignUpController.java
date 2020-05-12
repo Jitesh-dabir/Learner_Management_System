05992d16dbf0eb6f7e8c7497b0e1a88878f353a7
Jitesh-Dabir <jitudabir9@gmail.com>
2020-05-12 12:17:53 +0530

    Added: Controller for register users in learning management system

---
+++ src/main/java/com/bl/learningmanagementsystem/controller/SignUpController.java
@@ -0,0 +1,22 @@
+package com.bl.learningmanagementsystem.controller;
+
+import com.bl.learningmanagementsystem.dto.UserDTO;
+import com.bl.learningmanagementsystem.service.SignUpServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SignUpController {
+
+    @Autowired
+    private SignUpServiceImpl signUpService;
+
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public ResponseEntity<?> saveUser(@RequestBody UserDTO user) throws Exception {
+        return ResponseEntity.ok(signUpService.save(user));
+    }
+}
