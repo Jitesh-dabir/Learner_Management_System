bc2ab00945d97b59aa948d9f22ca5b630873fec6
Jitesh-Dabir <jitudabir9@gmail.com>
2020-05-12 12:12:36 +0530

    Added: Class for learning management system application

---
+++ src/main/java/com/bl/learningmanagementsystem/LearningManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.bl.learningmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningManagementSystemApplication.class, args);
+    }
+
+}
