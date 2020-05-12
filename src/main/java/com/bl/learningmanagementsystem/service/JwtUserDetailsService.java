37dc95630ef2a38744058b7cf006cab1fa25eb51
Jitesh-Dabir <jitudabir9@gmail.com>
2020-05-12 12:35:17 +0530

    Added: User details service layer to check user is valid or not by checking from database

---
+++ src/main/java/com/bl/learningmanagementsystem/service/JwtUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.bl.learningmanagementsystem.service;
+
+import java.util.ArrayList;
+
+import com.bl.learningmanagementsystem.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.bl.learningmanagementsystem.model.User;
+
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByFirstName(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found with username: " + username);
+        }
+        return new org.springframework.security.core.userdetails.User(user.getFirst_name(), user.getPassword(),
+                new ArrayList<>());
+    }
+}
