0c807fc921157ed02de13a5b73a85d0a911c0023
Jitesh-Dabir <jitudabir9@gmail.com>
2020-05-12 14:01:30 +0530

    Added: Files for configuration

---
+++ src/test/java/com/bl/learningmanagementsystem/LearningmanagementsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bl.learningmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningmanagementsystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
