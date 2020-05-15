3159bc21c516405b976acaf658858c8838a1de24
Jitesh-Dabir <jitudabir9@gmail.com>
2020-05-15 17:04:48 +0530

    Added: Class for store email get from request

---
+++ src/main/java/com/bl/learningmanagementsystem/dto/PasswordRequestModel.java
@@ -0,0 +1,14 @@
+package com.bl.learningmanagementsystem.dto;
+
+public class PasswordRequestModel {
+
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
