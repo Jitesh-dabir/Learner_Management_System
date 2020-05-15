b1ffa5117cb4385914de8e74ead86705012cba67
Jitesh-Dabir <jitudabir9@gmail.com>
2020-05-15 17:21:31 +0530

    Modified: Method to find user by email id

--- src/main/java/com/bl/learningmanagementsystem/repository/UserRepository.java
+++ src/main/java/com/bl/learningmanagementsystem/repository/UserRepository.java
@@ -5,8 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     @Query("select u from user_details u where u.first_name = ?1")
     public User findByFirstName(String first_name);
+
+    @Query("select u from user_details u where u.email = ?1")
+    User findByEmail(String email);
 }
