7bca82a23618adb97f3a33e1626c59dd67d65347
Jitesh-Dabir <jitudabir9@gmail.com>
2020-05-18 09:54:06 +0530

    Modified: Method to find details of user by name

--- src/main/java/com/bl/learningmanagementsystem/repository/HiredCandidateRepository.java
+++ src/main/java/com/bl/learningmanagementsystem/repository/HiredCandidateRepository.java
@@ -2,8 +2,13 @@ package com.bl.learningmanagementsystem.repository;
 
 import com.bl.learningmanagementsystem.model.HiredCandidateModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface HiredCandidateRepository extends JpaRepository<HiredCandidateModel, Integer> {
+    @Query("select h from hired_candidate h where h.first_name = ?1")
+    HiredCandidateModel findByFirst_name(String first_name);
 }
