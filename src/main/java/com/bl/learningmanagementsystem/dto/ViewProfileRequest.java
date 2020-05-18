9f6a0b55853388e5ef224af6564c4b72cfe78325
Jitesh-Dabir <jitudabir9@gmail.com>
2020-05-18 09:52:53 +0530

    Added: Class for view profile reqest dto

---
+++ src/main/java/com/bl/learningmanagementsystem/dto/ViewProfileRequest.java
@@ -0,0 +1,14 @@
+package com.bl.learningmanagementsystem.dto;
+
+public class ViewProfileRequest {
+
+    private String first_name;
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+}
