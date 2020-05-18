dd2775c1dc7bce31fc9d03a783b69bbda1def7aa
Jitesh-Dabir <jitudabir9@gmail.com>
2020-05-18 09:55:09 +0530

    Modified: Class name

--- src/main/java/com/bl/learningmanagementsystem/controller/ImportHiredCandidateController.java
+++ src/main/java/com/bl/learningmanagementsystem/controller/ImportHiredCandidateController.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 import java.util.List;
 
 @RestController
-public class HiredCandidateController {
+public class ImportHiredCandidateController {
 
     @Autowired
     HiredCandidateServiceImpl hiredCandidateService;
