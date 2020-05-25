c7c4b2c4b0d1cea7435d23ece93dcccadeaa1450
Jitesh-Dabir <jitudabir9@gmail.com>
2020-05-25 20:50:46 +0530

    Added: Class for database opration using Jpa

---
+++ src/main/java/com/bl/learningmanagementsystem/repository/FellowshipCandidateRepository.java
@@ -0,0 +1,7 @@
+package com.bl.learningmanagementsystem.repository;
+
+import com.bl.learningmanagementsystem.model.FellowshipCandidateModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FellowshipCandidateRepository extends JpaRepository<FellowshipCandidateModel,Integer> {
+}
