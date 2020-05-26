77aa4099b5982ebdb716efcc92abb1097d388e48
Jitesh-Dabir <jitudabir9@gmail.com>
2020-05-27 00:57:27 +0530

    Added: Endpoint to send job offer notification and join the candidate to company

--- src/main/java/com/bl/learningmanagementsystem/controller/FellowshipCandidateController.java
+++ src/main/java/com/bl/learningmanagementsystem/controller/FellowshipCandidateController.java
@@ -1,16 +1,15 @@
 package com.bl.learningmanagementsystem.controller;
 
 import com.bl.learningmanagementsystem.configuration.ApplicationConfiguration;
+import com.bl.learningmanagementsystem.dto.FellowshipCandidateDto;
 import com.bl.learningmanagementsystem.model.FellowshipCandidateModel;
 import com.bl.learningmanagementsystem.response.ResponseDto;
 import com.bl.learningmanagementsystem.service.IFellowshipCandidateService;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.mail.MessagingException;
 
