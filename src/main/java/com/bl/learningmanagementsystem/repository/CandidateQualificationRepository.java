d382a98fcb15963e29601ad616e41031d8b00c8d
Jitesh-Dabir <jitudabir9@gmail.com>
2020-05-27 20:10:47 +0530

    Added: Repository for perform operations with database

--- src/main/java/com/bl/learningmanagementsystem/repository/CandidateQualificationRepository.java
+++ src/main/java/com/bl/learningmanagementsystem/repository/CandidateQualificationRepository.java
@@ -1,4 +1,9 @@
 package com.bl.learningmanagementsystem.repository;
 
-public class CandidateQualificationRepository {
+import com.bl.learningmanagementsystem.model.CandidateQualificationModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CandidateQualificationRepository extends JpaRepository<CandidateQualificationModel, Long> {
 }
