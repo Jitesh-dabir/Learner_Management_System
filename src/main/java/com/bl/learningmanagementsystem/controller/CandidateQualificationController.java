800b35fa94999d82dbbf0c413913d05882fc74a3
Jitesh-Dabir <jitudabir9@gmail.com>
2020-05-27 20:13:10 +0530

    Added: Controller for update qualification information

---
+++ src/main/java/com/bl/learningmanagementsystem/controller/CandidateQualificationController.java
@@ -0,0 +1,32 @@
+package com.bl.learningmanagementsystem.controller;
+
+import com.bl.learningmanagementsystem.configuration.ApplicationConfiguration;
+import com.bl.learningmanagementsystem.dto.BankDetailsDto;
+import com.bl.learningmanagementsystem.dto.CandidateQualificationDto;
+import com.bl.learningmanagementsystem.model.BankDetailsModel;
+import com.bl.learningmanagementsystem.model.CandidateQualificationModel;
+import com.bl.learningmanagementsystem.response.ResponseDto;
+import com.bl.learningmanagementsystem.service.ICandidateQualificationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/candidatequalification")
+public class CandidateQualificationController {
+
+    @Autowired
+    private ICandidateQualificationService iCandidateQualificationService;
+
+    @PostMapping("/updatequalificationdetails")
+    public ResponseEntity<ResponseDto> updateQualificationDetails(@Valid @RequestBody CandidateQualificationDto candidateQualificationDto) {
+        CandidateQualificationModel updatedetails = iCandidateQualificationService.updateDetails(candidateQualificationDto);
+        return new ResponseEntity<>(new ResponseDto(updatedetails, ApplicationConfiguration.getMessageAccessor().getMessage("110")), HttpStatus.OK);
+    }
+}
