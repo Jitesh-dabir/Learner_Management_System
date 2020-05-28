ee52a99f27e76246a71183bdeebf908caf3b68d9
Jitesh-Dabir <jitudabir9@gmail.com>
2020-05-28 09:51:18 +0530

    Added: Method to handle exception like method not allowd

--- src/main/java/com/bl/learningmanagementsystem/exception/EntityExceptionHandler.java
+++ src/main/java/com/bl/learningmanagementsystem/exception/EntityExceptionHandler.java
@@ -2,8 +2,11 @@ package com.bl.learningmanagementsystem.exception;
 
 import com.bl.learningmanagementsystem.response.ErrorMessageDto;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,6 +14,7 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Date;
+import java.util.Set;
 
 @ControllerAdvice
 public class EntityExceptionHandler extends ResponseEntityExceptionHandler {
@@ -30,4 +34,13 @@ public class EntityExceptionHandler extends ResponseEntityExceptionHandler {
                 request.getDescription(false));
         return new ResponseEntity<>(errorObj, new HttpHeaders(), HttpStatus.BAD_REQUEST);
     }
+
+    //HttpRequestMethodNotSupportedException
+    @Override
+    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        ErrorMessageDto errorObj = new ErrorMessageDto(new Date(), "FromHttpRequestMethodNotSupportedException-Method not allowed",
+                request.getDescription(false));
+        return new ResponseEntity<>(errorObj, new HttpHeaders(), HttpStatus.METHOD_NOT_ALLOWED);
+    }
+
 }
