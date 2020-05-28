a33f3de83c9795c03be48b71e1c8748cf1ce4491
Jitesh-Dabir <jitudabir9@gmail.com>
2020-05-28 09:31:11 +0530

    Added: Method for handle exception for method arguments

--- src/main/java/com/bl/learningmanagementsystem/exception/EntityExceptionHandler.java
+++ src/main/java/com/bl/learningmanagementsystem/exception/EntityExceptionHandler.java
@@ -1,9 +1,10 @@
 package com.bl.learningmanagementsystem.exception;
 
-import com.bl.learningmanagementsystem.dto.ErrorMessageDto;
+import com.bl.learningmanagementsystem.response.ErrorMessageDto;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -14,10 +15,20 @@ import java.util.Date;
 @ControllerAdvice
 public class EntityExceptionHandler extends ResponseEntityExceptionHandler {
 
+    //Custom exception in global exception handler
     @ExceptionHandler(LmsAppServiceException.class)
     public final ResponseEntity<ErrorMessageDto> handleAnyException(Exception ex, WebRequest request) {
         ErrorMessageDto errorObj = new ErrorMessageDto(new Date(), ex.getMessage(),
                 request.getDescription(false));
         return new ResponseEntity<>(errorObj, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
+    //MethodArgumentNotValidException
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        ErrorMessageDto errorObj = new ErrorMessageDto(new Date(), "From method argument not valid exception",
+                request.getDescription(false));
+        return new ResponseEntity<>(errorObj, new HttpHeaders(), HttpStatus.BAD_REQUEST);
+    }
+
 }
