dac36e2fb57ceedc976df69f69c256ea1fa2db1a
Jitesh-Dabir <jitudabir9@gmail.com>
2020-05-28 09:31:41 +0530

    Modified: Change package

---
+++ src/main/java/com/bl/learningmanagementsystem/response/ErrorMessageDto.java
@@ -0,0 +1,38 @@
+package com.bl.learningmanagementsystem.response;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorMessageDto {
+    private Date timestamp;
+    private String message;
+    private String details;
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+}
