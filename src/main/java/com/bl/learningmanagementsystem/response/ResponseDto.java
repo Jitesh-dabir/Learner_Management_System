42e0322ce994fb37c8829ed4982602de04a39aef
Jitesh-Dabir <jitudabir9@gmail.com>
2020-05-30 10:09:59 +0530

    Added: Status field in response dto

--- src/main/java/com/bl/learningmanagementsystem/response/ResponseDto.java
+++ src/main/java/com/bl/learningmanagementsystem/response/ResponseDto.java
@@ -3,10 +3,12 @@ package com.bl.learningmanagementsystem.response;
 public class ResponseDto {
 
     public Object data;
+    public int status;
     public String message;
 
-    public ResponseDto(Object data, String message) {
+    public ResponseDto(Object data, int status, String message) {
         this.data = data;
         this.message = message;
+        this.status = status;
     }
 }
