4574c2b0c322f5b8d7bb5d129043081415c3d514
Jitesh-Dabir <jitudabir9@gmail.com>
2020-05-30 09:37:54 +0530

    Modified: Configuration for swagger

--- src/main/java/com/bl/learningmanagementsystem/configuration/SwaggerConfig.java
+++ src/main/java/com/bl/learningmanagementsystem/configuration/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.bl.learningmanagementsystem.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -9,7 +10,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfiguration {
+public class SwaggerConfig {
 
     @Bean
     public Docket postsApi() {
@@ -17,6 +18,7 @@ public class SwaggerConfiguration {
                 .host("8084")
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.bl.learningmanagementsystem"))
+                .paths(PathSelectors.any())
                 .build();
     }
 }
