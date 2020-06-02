922eb9a7a1f1d009224030b1b14fd2e6d1ec3f9c
Jitesh-Dabir <jitudabir9@gmail.com>
2020-06-02 10:48:47 +0530

    Modified: Remove unwanted files

--- src/main/java/com/bl/learningmanagementsystem/service/IBankDetailsService.java
+++
@@ -1,8 +0,0 @@
-package com.bl.learningmanagementsystem.service;
-
-import com.bl.learningmanagementsystem.dto.BankDetailsDto;
-import com.bl.learningmanagementsystem.model.BankDetailsModel;
-
-public interface IBankDetailsService {
-    BankDetailsModel updatedetails(BankDetailsDto bankDetailsDto);
-}
--- src/main/java/com/bl/learningmanagementsystem/service/ICandidateQualificationService.java
+++
@@ -1,8 +0,0 @@
-package com.bl.learningmanagementsystem.service;
-
-import com.bl.learningmanagementsystem.dto.CandidateQualificationDto;
-import com.bl.learningmanagementsystem.model.CandidateQualificationModel;
-
-public interface ICandidateQualificationService {
-    CandidateQualificationModel updateDetails(CandidateQualificationDto candidateQualificationDto);
-}
