392653cae9d27643c3facbc3896880b6a09c02a7
Jitesh-Dabir <jitudabir9@gmail.com>
2020-06-02 11:18:05 +0530

    Added: Comments to all methods

--- src/main/java/com/bl/learningmanagementsystem/controller/UserController.java
+++ src/main/java/com/bl/learningmanagementsystem/controller/UserController.java
@@ -26,30 +26,58 @@ public class UserController {
     @Autowired
     private IUserDetailsService userDetailsService;
 
+    /**
+     *
+     * @param userDto
+     * @return response(Registered user)
+     */
     @PostMapping("/register")
     public ResponseEntity<ResponseDto> saveUser(@Valid @RequestBody UserDto userDto) {
         User user = userDetailsService.save(userDto);
         return new ResponseEntity<>(new ResponseDto(user, 200, ApplicationConfiguration.getMessageAccessor().getMessage("101")), HttpStatus.CREATED);
     }
 
+    /**
+     *
+     * @param authenticationRequest
+     * @return response(token)
+     * @throws Exception
+     */
     @PostMapping("/authenticate")
     public ResponseEntity<JwtResponseDto> createAuthenticationToken(@RequestBody JwtRequestDto authenticationRequest) throws Exception {
         String token = userDetailsService.getAuthenticationToken(authenticationRequest);
         return new ResponseEntity<>(new JwtResponseDto(token, ApplicationConfiguration.getMessageAccessor().getMessage("106")), HttpStatus.CREATED);
     }
 
+    /**
+     *
+     * @param loginDto
+     * @return response(userId)
+     */
     @GetMapping("/login")
     public ResponseEntity<ResponseDto> login(@Valid @RequestBody LoginDto loginDto) {
         long userId = userDetailsService.loginUser(loginDto);
         return new ResponseEntity<>(new ResponseDto(userId, 200, ApplicationConfiguration.getMessageAccessor().getMessage("102")), HttpStatus.OK);
     }
 
+    /**
+     *
+     * @param email
+     * @return resetPasswordToken
+     * @throws MessagingException
+     * @throws LmsAppServiceException
+     */
     @GetMapping("/requestresetpassword")
     public ResponseEntity<ResponseDto> requestResetPassword(@Valid @RequestParam(value = "email") String email) throws MessagingException, LmsAppServiceException {
         String resetPasswordToken = userDetailsService.getResetPasswordToken(email);
         return new ResponseEntity<>(new ResponseDto(resetPasswordToken, 200,ApplicationConfiguration.getMessageAccessor().getMessage("103")), HttpStatus.ACCEPTED);
     }
 
+    /**
+     *
+     * @param passwordRequestModel
+     * @return response(Reseted password)
+     */
     @PutMapping("/resetpassword")
     public ResponseEntity<ResponseDto> resetPassword(@Valid @RequestBody PasswordResetModelDto passwordRequestModel) {
         User user = userDetailsService.resetPassword(passwordRequestModel.getPassword(), passwordRequestModel.getToken());
