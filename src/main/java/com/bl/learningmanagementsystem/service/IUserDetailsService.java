e8d15b0d0df3b3d8d9738d5f242bb51402ae16d7
Jitesh-Dabir <jitudabir9@gmail.com>
2020-06-10 12:27:36 +0530

    Modified: Token generate and store in redis when login

--- src/main/java/com/bl/learningmanagementsystem/service/IUserDetailsService.java
+++ src/main/java/com/bl/learningmanagementsystem/service/IUserDetailsService.java
@@ -1,25 +1,26 @@
 package com.bl.learningmanagementsystem.service;
 
-import com.bl.learningmanagementsystem.dto.JwtRequestDto;
 import com.bl.learningmanagementsystem.dto.LoginDto;
 import com.bl.learningmanagementsystem.dto.UserDto;
 import com.bl.learningmanagementsystem.exception.LmsAppServiceException;
 import com.bl.learningmanagementsystem.model.User;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import javax.mail.MessagingException;
+import java.util.Map;
 
 public interface IUserDetailsService {
     User save(UserDto user);
 
     User resetPassword(String password, String token);
 
-    String getResetPasswordToken(String email) throws MessagingException, LmsAppServiceException;
+    String getResetPasswordToken(String email) throws MessagingException, LmsAppServiceException, JsonProcessingException;
 
-    String getAuthenticationToken(JwtRequestDto authenticationRequest) throws Exception;
+    String getAuthenticationToken(String userName, String password) throws Exception;
 
     UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
 
-    long loginUser(LoginDto loginDto);
+    Map<Object, Object> loginUser(LoginDto loginDto) throws Exception;
 }
--- src/main/java/com/bl/learningmanagementsystem/service/UserDetailsServiceImpl.java
+++ src/main/java/com/bl/learningmanagementsystem/service/UserDetailsServiceImpl.java
@@ -1,16 +1,20 @@
 package com.bl.learningmanagementsystem.service;
 
-import com.bl.learningmanagementsystem.dto.JwtRequestDto;
+import com.bl.learningmanagementsystem.dto.EmailDto;
 import com.bl.learningmanagementsystem.dto.LoginDto;
 import com.bl.learningmanagementsystem.dto.UserDto;
 import com.bl.learningmanagementsystem.exception.LmsAppServiceException;
 import com.bl.learningmanagementsystem.model.User;
 import com.bl.learningmanagementsystem.repository.UserRepository;
+import com.bl.learningmanagementsystem.util.IRabbitMq;
 import com.bl.learningmanagementsystem.util.JwtTokenUtil;
+import com.bl.learningmanagementsystem.util.RabbitMqUtil;
+import com.bl.learningmanagementsystem.util.RedisUtil;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
@@ -22,11 +26,14 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
 import java.util.ArrayList;
+import java.util.Map;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService, IUserDetailsService {
+
+    @Value("${spring.redis.key}")
+    private static final String REDIS_INDEX_KEY = "LMS_TOKEN";
 
     @Autowired
     private UserRepository userRepository;
@@ -49,6 +56,12 @@ public class UserDetailsServiceImpl implements UserDetailsService, IUserDetailsS
     @Autowired
     private JavaMailSender sender;
 
+    @Autowired
+    private RedisUtil redisUtil;
+
+    @Autowired
+    private IRabbitMq rabbitMq;
+
     /**
      * @param username
      * @return response(Method to load user by its username from database.)
@@ -56,9 +69,9 @@ public class UserDetailsServiceImpl implements UserDetailsService, IUserDetailsS
      */
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByFirstName(username).orElseThrow(() -> new LmsAppServiceException(LmsAppServiceException.exceptionType
+        User user = userRepository.findByEmail(username).orElseThrow(() -> new LmsAppServiceException(LmsAppServiceException.exceptionType
                 .USER_NOT_FOUND, "User not found with username"));
-        return new org.springframework.security.core.userdetails.User(user.getFirstName(), user.getPassword(),
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(),
                 new ArrayList<>());
     }
 
@@ -67,12 +80,33 @@ public class UserDetailsServiceImpl implements UserDetailsService, IUserDetailsS
      * @return response(Method to valid user details and allow for login)
      */
     @Override
-    public long loginUser(LoginDto loginDto) {
-        User user = userRepository.findByEmail(loginDto.email).orElseThrow(() -> new LmsAppServiceException(LmsAppServiceException.exceptionType
+    public Map<Object, Object> loginUser(LoginDto loginDto) throws Exception {
+        User user = userRepository.findByEmail(loginDto.getEmail()).orElseThrow(() -> new LmsAppServiceException(LmsAppServiceException.exceptionType
                 .INVALID_EMAIL_ID, "User not found with email"));
-        if (!bcryptEncoder.matches(loginDto.password, user.getPassword()))
-            throw new LmsAppServiceException(LmsAppServiceException.exceptionType.INVALID_PASSWORD, "Invalid password");
-        return user.getId();
+        String authenticationToken = getAuthenticationToken(user.getEmail(), loginDto.getPassword());
+        redisUtil.save(REDIS_INDEX_KEY, user.getEmail(), authenticationToken);
+        return redisUtil.getAll(REDIS_INDEX_KEY);
+    }
+
+    /**
+     * @param userName, password
+     * @return Method to get authentication token
+     * @throws Exception
+     */
+    @Override
+    public String getAuthenticationToken(String userName, String password) throws Exception {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                    userName, password));
+            final UserDetails userDetails = userDetailsService
+                    .loadUserByUsername(userName);
+            final String token = jwtTokenUtil.generateToken(userDetails);
+            return token;
+        } catch (DisabledException e) {
+            throw new Exception("USER_DISABLED", e);
+        } catch (BadCredentialsException e) {
+            throw new Exception("INVALID_CREDENTIALS", e);
+        }
     }
 
     /**
@@ -115,7 +149,7 @@ public class UserDetailsServiceImpl implements UserDetailsService, IUserDetailsS
      * @throws LmsAppServiceException
      */
     @Override
-    public String getResetPasswordToken(String email) throws MessagingException, LmsAppServiceException {
+    public String getResetPasswordToken(String email) throws MessagingException, LmsAppServiceException, JsonProcessingException {
         User user = userRepository.findByEmail(email).orElseThrow(() -> new LmsAppServiceException(LmsAppServiceException.exceptionType
                 .INVALID_EMAIL_ID, "User not found with email"));
         final String token = jwtTokenUtil.generatePasswordResetToken(String.valueOf(user.getId()));
@@ -124,47 +158,17 @@ public class UserDetailsServiceImpl implements UserDetailsService, IUserDetailsS
     }
 
     /**
-     * @param authenticationRequest
-     * @return Method to get authentication token
-     * @throws Exception
-     */
-    @Override
-    public String getAuthenticationToken(JwtRequestDto authenticationRequest) throws Exception {
-        authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
-        final UserDetails userDetails = userDetailsService
-                .loadUserByUsername(authenticationRequest.getUsername());
-        final String token = jwtTokenUtil.generateToken(userDetails);
-        return token;
-    }
-
-    /**
-     * @param username, password
-     * @return Method to authenticate user.
-     * @throws Exception
-     */
-    private void authenticate(String username, String password) throws Exception {
-        try {
-            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
-        } catch (DisabledException e) {
-            throw new Exception("USER_DISABLED", e);
-        } catch (BadCredentialsException e) {
-            throw new Exception("INVALID_CREDENTIALS", e);
-        }
-    }
-
-    /**
      * @param user, token
      * @return Method to send reset password request email.
      * @throws MessagingException
      */
-    public void sentEmail(User user, String token) throws MessagingException {
-        String recipientAddress = user.getEmail();
-        MimeMessage message = sender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(message);
-        helper.setTo(recipientAddress);
-        helper.setText("Hii " + user.getFirstName() + "\n" + " You requested to reset password, if YES then click on link put your new password and NO then ignore \n" +
+    public void sentEmail(User user, String token) throws MessagingException, JsonProcessingException {
+        EmailDto emailDto = new EmailDto();
+        emailDto.setTo(user.getEmail());
+        emailDto.setSubject("Password-Reset-Request");
+        emailDto.setText("Hii " + user.getFirstName() + "\n" + " You requested to reset password, if YES then click on link put your new password and NO then ignore \n" +
                 "http://localhost:8084/reset_password?json={%22password%22:%22" + null + "%22+,%22token%22:" + token + "}");
-        helper.setSubject("Password-Reset-Request");
-        sender.send(message);
+       rabbitMq.send(emailDto);
+       //rabbitMq.sendMail(emailDto);
     }
 }
