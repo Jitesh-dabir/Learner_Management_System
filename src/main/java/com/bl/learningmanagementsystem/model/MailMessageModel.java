382bec20c514295a3bed9d3c64181783d8f08287
Jitesh-Dabir <jitudabir9@gmail.com>
2020-06-10 11:55:13 +0530

    Added: Class to map email details message listen from queue

---
+++ src/main/java/com/bl/learningmanagementsystem/model/MailMessageModel.java
@@ -0,0 +1,30 @@
+package com.bl.learningmanagementsystem.model;
+
+
+import com.alibaba.fastjson.annotation.JSONField;
+import lombok.Data;
+
+@Data
+public class MailMessageModel {
+    @JSONField(name = "from")
+    private String from;
+
+    @JSONField(name = "to")
+    private String to;
+
+    @JSONField(name = "subject")
+    private String subject;
+
+    @JSONField(name = "text")
+    private String text;
+
+    @Override
+    public String toString() {
+        StringBuffer sb = new StringBuffer();
+        sb.append("Email{from:").append(this.from).append(", ");
+        sb.append("to:").append(this.to).append(", ");
+        sb.append("subject:").append(this.subject).append(", ");
+        sb.append("text:").append(this.text).append("}");
+        return sb.toString();
+    }
+}
