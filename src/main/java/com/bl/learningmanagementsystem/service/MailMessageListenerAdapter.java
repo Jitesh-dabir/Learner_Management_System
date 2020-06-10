37be05a657af759ab37c9984cbdf1e8ced4e9b64
Jitesh-Dabir <jitudabir9@gmail.com>
2020-06-10 11:59:24 +0530

    Added: Class for listen message from rabbitmq queue and send mail to that email details

---
+++ src/main/java/com/bl/learningmanagementsystem/service/MailMessageListenerAdapter.java
@@ -0,0 +1,56 @@
+package com.bl.learningmanagementsystem.service;
+
+import com.alibaba.fastjson.JSONObject;
+import com.bl.learningmanagementsystem.model.MailMessageModel;
+import com.rabbitmq.client.Channel;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import javax.mail.internet.MimeMessage;
+
+@Component("mailMessageListenerAdapter")
+public class MailMessageListenerAdapter extends MessageListenerAdapter {
+
+    @Resource
+    private JavaMailSender mailSender;
+
+    /**
+     *
+     * @param message
+     * @param channel
+     */
+    @Override
+    public void onMessage(Message message, Channel channel) {
+        System.out.println(message.getMessageProperties().getConsumerQueue());
+        try {
+            String messageBody = new String(message.getBody());
+            MailMessageModel mailMessageModel = JSONObject.toJavaObject(JSONObject.parseObject(messageBody), MailMessageModel.class);
+            String to =  mailMessageModel.getTo();
+            String subject = mailMessageModel.getSubject();
+            String text = mailMessageModel.getText();
+            sendHtmlMail(to, subject, text);
+            channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * @param to
+     * @param subject
+     * @param text
+     * @throws Exception
+     */
+    private void sendHtmlMail(String to, String subject, String text) throws Exception {
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage);
+        mimeMessageHelper.setTo(to);
+        mimeMessageHelper.setSubject(subject);
+        mimeMessageHelper.setText(text, true);
+        mailSender.send(mimeMessage);
+    }
+}
