27b0a2444d9265f7f34f6a97e11b114257032470
Jitesh-Dabir <jitudabir9@gmail.com>
2020-06-13 12:10:22 +0530

    Added: Comments for redis methods

--- src/main/java/com/bl/learningmanagementsystem/util/RedisUtil.java
+++ src/main/java/com/bl/learningmanagementsystem/util/RedisUtil.java
@@ -8,21 +8,42 @@ import java.io.Serializable;
 import java.util.Map;
 
 @Component
-public class RedisUtil implements Serializable {
+public class RedisUtil implements Serializable, IRedisUtil{
 
     private static final long serialVersionUID = 1L;
 
     @Autowired
     private RedisTemplate<String, Object> redisTemplate;
 
+    /**
+     *
+     * @param RedisKey
+     * @param email
+     * @param authenticationToken
+     * Save token for user
+     */
+    @Override
     public void save(String RedisKey, String email, String authenticationToken) {
         redisTemplate.opsForHash().put(RedisKey, email, authenticationToken);
     }
 
+    /**
+     *
+     * @param redisKey
+     * @return all token
+     */
+    @Override
     public Map<Object, Object> getAll(String redisKey) {
         return redisTemplate.opsForHash().entries(redisKey);
     }
 
+    /**
+     *
+     * @param redisKey
+     * @param userId
+     * @return Token
+     */
+    @Override
     public Map<Object, Object> getToken(String redisKey, String userId) {
         return (Map<Object, Object>) redisTemplate.opsForHash().get(redisKey, userId);
     }
